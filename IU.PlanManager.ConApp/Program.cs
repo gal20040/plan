4a955d254ed4490636b857bd86f484f7d38b21d8
Artem Grodeckiy <gal20040@gmail.com>
2019-07-02 10:58:37 +0300

    Добавил начальный проект

---
+++ IU.PlanManager.ConApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IU.PlanManager.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
