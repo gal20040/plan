d759498d7d9334e02352585e8aa6a01bd42f8a6b
Artem Grodeckiy <gal20040@gmail.com>
2019-07-03 21:53:36 +0300

    Добавил конструкторы для Event

--- IU.PlanManager.ConApp/Models/Event.cs
+++ IU.PlanManager.ConApp/Models/Event.cs
@@ -4,9 +4,70 @@ namespace IU.PlanManager.ConApp.Models
 {
     public class Event : IEntity
     {
-        public Event()
+        private Event()
         {
             Guid = Guid.NewGuid();
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="description">Описание</param>
+        public Event(string title, string description) : this()
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("message", nameof(title)); //todo добавить message
+            }
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("message", nameof(description)); //todo добавить message
+            }
+
+            Title = title;
+            Description = description;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="description">Описание</param>
+        /// <param name="startDateTime">Начало события</param>
+        public Event(string title, string description, DateTime? startDateTime) : this(title, description)
+        {
+            StartDateTime = startDateTime;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="description">Описание</param>
+        /// <param name="startDateTime">Начало события</param>
+        /// <param name="endDateTime">Окончание события</param>
+        public Event(string title, string description, DateTime? startDateTime, DateTime? endDateTime) : this(title, description, startDateTime)
+        {
+            if (startDateTime == null && endDateTime != null)
+            {
+                throw new ArgumentNullException(nameof(startDateTime), "message"); //todo добавить message
+            }
+
+            EndDateTime = endDateTime;
+        }
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="title">Заголовок</param>
+        /// <param name="description">Описание</param>
+        /// <param name="startDateTime">Начало события</param>
+        /// <param name="endDateTime">Окончание события</param>
+        public Event(string title, string description, DateTime? startDateTime, DateTime? endDateTime, string place) : this(title, description, startDateTime, endDateTime)
+        {
+            Place = place;
         }
 
         /// <summary>
@@ -38,5 +99,18 @@ namespace IU.PlanManager.ConApp.Models
         /// Место
         /// </summary>
         public string Place { get; set; }
+
+        /// <summary>
+        /// Место
+        /// </summary>
+        public override string ToString()
+        {
+            return $"Guid:\t\t{Guid}\n" +
+                   $"Title:\t\t{Title}\n" +
+                   $"Description:\t{Description}\n" +
+                   $"Place:\t\t{Place}\n" +
+                   $"StartDateTime:\t{StartDateTime}\n" +
+                   $"EndDateTime:\t{EndDateTime}";
+        }
     }
 }
