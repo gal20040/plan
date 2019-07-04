74175c418ba5f8d241c8d7d70efd5d3d28931e87
Artem Grodeckiy <gal20040@gmail.com>
2019-07-04 09:29:03 +0300

    Добавил сущность User

---
+++ IU.PlanManager.ConApp/Models/User.cs
@@ -0,0 +1,62 @@
+﻿using IU.PlanManager.ConApp.Interfaces;
+using System;
+
+namespace IU.PlanManager.ConApp.Models
+{
+    public class User : IEntity
+    {
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// День рождения
+        /// </summary>
+        public string Birthday { get; set; }
+
+        /// <summary>
+        /// Фотография
+        /// </summary>
+        public string Photo { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Принимать приглашение
+        /// </summary>
+        public bool AllowInvites { get; set; }
+
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Gender Gender { get; set; }
+    }
+
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Не определён
+        /// </summary>
+        Undefined,
+        /// <summary>
+        /// Женский
+        /// </summary>
+        Female,
+        /// <summary>
+        /// Мужской
+        /// </summary>
+        Male
+    }
+}
