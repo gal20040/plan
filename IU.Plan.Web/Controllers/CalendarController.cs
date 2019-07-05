26e05bf7299f882b3ee1e8bbaacb19bf4bb0fd20
Artem Grodeckiy <gal20040@gmail.com>
2019-07-05 21:37:33 +0400

    Добавил страницу с календарём.

---
+++ IU.Plan.Web/Controllers/CalendarController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using IU.PlanManager.Core.Impl;
+using System.Web.Mvc;
+
+namespace IU.Plan.Web.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            var eventFileStore = new EventFileStore();
+            var today = DateTime.Now;
+            var firstDayOfMonth = new DateTime(today.Year, today.Month, 1);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            var model = eventFileStore.Entities.Where(evt => evt.StartDateTime >= firstDayOfMonth && evt.StartDateTime <= lastDayOfMonth);
+
+            return View(model);
+        }
+    }
+}
---
+++ IU.Plan.Web/Models/CalendarViewModel.cs
@@ -0,0 +1,18 @@
+﻿using IU.PlanManager.Core.Models;
+using System.Collections.Generic;
+
+namespace IU.Plan.Web.Models
+{
+    public class CalendarViewModel
+    {
+        public CalendarViewModel()
+        {
+            Events = new List<Event>();
+        }
+
+        public IEnumerable<Event> Events { get; set; }
+        public int RowCount { get; set; }
+        public int ColCount { get; set; }
+        public int Limit { get; set; }
+    }
+}
