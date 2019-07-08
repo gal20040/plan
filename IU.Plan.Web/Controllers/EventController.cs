797eae9d31e202a032b159311f7b78f881dc976e
Artem Grodeckiy <gal20040@gmail.com>
2019-07-08 23:20:23 +0400

    Добавил вьюшку для просмотра события.

---
+++ IU.Plan.Web/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using IU.PlanManager.Core.Impl;
+using IU.PlanManager.Core.Interfaces;
+using IU.PlanManager.Core.Models;
+
+namespace IU.Plan.Web.Controllers
+{
+    public class EventController : Controller
+    {
+        private IStore<Event> store = new EventFileStore();
+
+        // GET: Event
+        public ActionResult Details(Guid uid)
+        {
+            var evnt = store.Get(uid);
+
+            return View(evnt);
+        }
+
+        public PartialViewResult View(Guid uid)
+        {
+            var evnt = store.Get(uid);
+
+            return PartialView("Details", evnt);
+        }
+    }
+}
