785e6aafe1676b753dd34615adc2559ca759d19d
Artem Grodeckiy <gal20040@gmail.com>
2019-07-09 01:13:43 +0400

    Добавил расширение для DayOfWeek

---
+++ IU.Plan.Web/Extensions/DayOfWeekExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IU.Plan.Web.Extensions
+{
+    public static class DayOfWeekExtension
+    {
+        /// <summary>
+        /// Возвращает числовой код дня от 0 до 6
+        /// Неделя начинается с Понедельника
+        /// </summary>
+        /// <param name="dayOfWeek">День недели</param>
+        /// <returns></returns>
+        public static int ToInt(this DayOfWeek dayOfWeek)
+        {
+            return dayOfWeek == 0 ? 7 : (int)dayOfWeek - 1;
+        }
+    }
+}
