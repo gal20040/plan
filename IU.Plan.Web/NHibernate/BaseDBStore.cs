e1334838537e4e53a6368ff2d379f07cdd4fa412
Artem Grodeckiy <gal20040@gmail.com>
2019-07-25 04:26:50 +0400

    Добавил загрузку данных в БД из json (если БД пустая).

--- IU.Plan.Web/NHibernate/BaseDBStore.cs
+++ IU.Plan.Web/NHibernate/BaseDBStore.cs
@@ -1,15 +1,26 @@
 ﻿using IU.Plan.Core.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace IU.Plan.Web.NHibernate
 {
     /// <summary>
     /// Хранилище событий <see cref="IEntity"/>
     /// </summary>
-    public class BaseDBStore<T> : IStore<T>
-        where T : class, IEntity
+    public class BaseDBStore<T> : IStore<T> where T : class, IEntity
     {
+        private string fileName = AppDomain.CurrentDomain.BaseDirectory + @"..\{0}.json";
+
+        public BaseDBStore()
+        {
+            if (GetRowsCount() == 0)
+            {
+                Init();
+            }
+        }
+
         public virtual IEnumerable<T> Entities
         {
             get
@@ -38,10 +49,10 @@ namespace IU.Plan.Web.NHibernate
                     if (entity != null)
                     {
                         session.Delete(entity);
+                        result = true;
                     }
                     tx.Commit();
                 }
-                result = true;
             }
             finally
             {
@@ -57,6 +68,27 @@ namespace IU.Plan.Web.NHibernate
             return session.Load<T>(uid);
         }
 
+        public int GetRowsCount()
+        {
+            var session = NHibernateHelper.GetCurrentSession();
+
+            return session.QueryOver<T>().List().Count;
+        }
+
+        protected virtual void Init()
+        {
+            fileName = string.Format(fileName, typeof(T).Name.ToLower());
+            if (File.Exists(fileName))
+            {
+                var file = File.ReadAllText(fileName);
+                var entities = JsonConvert.DeserializeObject<T[]>(file);
+                foreach (var entity in entities)
+                {
+                    SaveByGuid(entity);
+                }
+            }
+        }
+
         public void UpdateByGuid(T entity)
         {
             var session = NHibernateHelper.GetCurrentSession();
@@ -65,6 +97,23 @@ namespace IU.Plan.Web.NHibernate
                 using (var tx = session.BeginTransaction())
                 {
                     session.SaveOrUpdate(entity);
+                    tx.Commit();
+                }
+            }
+            finally
+            {
+                NHibernateHelper.CloseSession();
+            }
+        }
+
+        public void SaveByGuid(T entity)
+        {
+            var session = NHibernateHelper.GetCurrentSession();
+            try
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    session.Save(entity);
                     tx.Commit();
                 }
             }
